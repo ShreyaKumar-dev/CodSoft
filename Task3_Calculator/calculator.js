9ecc187ec6fbdfa61b2fd077bec9809ac8e82042
ShreyaKumar-dev <shreyatiwary2003@gmail.com>
2024-01-05 22:32:35 +0530

    Project Completed

--- Task3_Calculator/calculator.js
+++ Task3_Calculator/calculator.js
@@ -1,22 +1,64 @@
-let res = document.getElementById("display");
+// let res = document.getElementById("display");
 
-let calculate=(num)=>{
-    res.value += num; 
-}
+// let calculate=(num)=>{
+//     res.value += num; 
+// }
 
-let result=()=>{
-    try{
-        res.value = eval(res.value);
+// let result=()=>{
+//     try{
+//         res.value = eval(res.value);
+//     }
+//     catch(err){
+//         alert("Enter the valid input")
+//     }
+// }
+
+// let clrDisplay=()=>{
+//     res.value = "0";
+// }
+
+// let back=()=>{
+//     res.value = res.value.slice(0,-1);
+// }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const display = document.getElementById('display');
+    const buttons = document.querySelectorAll('button');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+            const buttonVal = this.innerText;
+            
+            if (buttonVal === 'AC') {
+                clrDisplay();
+            } else if (buttonVal === 'DEL') {
+                back();
+            } else if (buttonVal === '=') {
+                res();
+            } else {
+                txtDisplay(buttonVal);
+            }
+        });
+    });
+
+    function clrDisplay() {
+        display.value = '';
     }
-    catch(err){
-        alert("Enter the valid input")
+
+    function back() {
+        display.value = display.value.slice(0, -1);
     }
-}
 
-let clrDisplay=()=>{
-    res.value = "0";
-}
+    function txtDisplay(num) {
+        display.value += num;
+    }
 
-let back=()=>{
-    res.value = res.value.slice(0,-1);
-}
+    function res() {
+        try {
+            display.value = eval(display.value);
+        } catch (error) {
+            display.value = 'Error';
+        }
+    }
+});
+
